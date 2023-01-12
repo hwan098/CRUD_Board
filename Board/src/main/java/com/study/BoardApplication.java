65ef13ae6ceee75bec25414e78b92a816e4a1498
hwan098 <glqkfl7845@gmail.com>
2023-01-12 09:17:22 +0900

    src/main/java 디렉터리 BoardApplication 클래스 설명 main메서드, @SpringBootApplication

--- Board/src/main/java/com/study/BoardApplication.java
+++ Board/src/main/java/com/study/BoardApplication.java
@@ -11,3 +11,12 @@ public class BoardApplication {
 	}
 
 }
+
+/*
+ * main메서드는 SpringApplication.run()을 호출해서  웹 어플리케이션을 실행하는 역할을 한다.
+ * @SpringBootApplication 애너테이션 구성
+ * @EnableAutoConfiguration : 개발에 필요한 필수적인 설정들의 처리가 되어있다. 해당 애너테이션으로 다양한 설정들의 일부가 자동으로 완료된다.
+ * @ComponentScan : 기존의 xml설정 방식의 스프링은 빈의 등록 및 스캔을 위해서 수동으로 ComponentScan을 여러 개 선언하는 방식을 사용했는데 
+ * 스프링부트는 해당 애너테이션에 의해서 자동으로 컴포넌트 클래스를 검색하고 스프링 애플리케이션 콘텍스트에 빈으로 등록한다. 쉽게 말해 의존성 주입과정이 더욱 간편해졌다.
+ * @Configuration : 해당 애너테이션이 선언된 클래스는 자바 기반의 설정 파일로 인식된다.
+ * */
