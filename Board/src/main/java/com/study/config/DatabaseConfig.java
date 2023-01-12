aad0e87c857eb7973ee3b45b51a98a04e91a1d20
hwan098 <glqkfl7845@gmail.com>
2023-01-12 10:52:12 +0900

    DatabaseConfig어노테이션 설명 추가 - 컨티이너에 등록된 빈의 의존성 주입

--- Board/src/main/java/com/study/config/DatabaseConfig.java
+++ Board/src/main/java/com/study/config/DatabaseConfig.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -14,20 +15,21 @@ import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
-@Configuration
-@PropertySource("classpath:/application.properties")
+@Configuration	// @Configuration이 지정된 클래스를 자바 기반의 설정 파일로 인식
+@PropertySource("classpath:/application.properties")	//해당 클래스에서 참조할 properties 파일의 위치를 지정
 public class DatabaseConfig {
 
-    @Autowired
+    @Autowired	//Bean으로 등록된 인스턴스를 클래스에 주입하는데 사용
     private ApplicationContext context;
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")	// @PropertySource에 지정된 파일(application.properties)에서 prefix에 해당하는 spring.datasource.hikari로 시작하는 설정을 모두 읽어 들여 해당 메서드에 매핑(바인딩).
+
     public HikariConfig hikariConfig() {
         return new HikariConfig();
     }
 
-    @Bean
+    @Bean	//Configuration 클래스의 메서드레벨에만 지정 가능, @Bean이 지정된 객체는 컨테이너에 의해 관리되는 Bean으로 등록됨
     public DataSource dataSource() {
         return new HikariDataSource(hikariConfig());
     }
@@ -53,3 +55,7 @@ public class DatabaseConfig {
     }
 
 }
+/*
+ * 
+ * */
+ 
